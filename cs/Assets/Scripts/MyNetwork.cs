c52e15a20fb66d035d9242be2caaa6328f2fca43
hackerff <1141524967@qq.com>
2018-03-04 21:56:08 +0800

    new file:   .vs/cs/v14/.suo

---
+++ cs/Assets/Scripts/MyNetwork.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyNetwork : MonoBehaviour
+{
+
+    public int connections = 10;
+    public int listenPort = 8899;
+    public bool nat = false;
+    // Use this for initialization
+    void OnGUI()
+    {
+        if (Network.peerType == NetworkPeerType.Disconnected)
+        {
+            if (GUILayout.Button("创建服务器"))
+            {
+                NetworkConnectionError error = Network.InitializeServer(connections, listenPort, nat);
+                print(error);
+            }
+            if (GUILayout.Button("链接服务器"))
+            {
+
+            }
+        }
+        else if (Network.peerType == NetworkPeerType.Server)
+        {
+            GUILayout.Label("服务器创建完成");
+        }
+
+    }
+}
